64862a5ed8c5707d612792d032475e792aa32d4c
Ratstar216 <130824137+Ratstar216@users.noreply.github.com>
2024-12-16 01:17:46 +0900

    update

--- gemini/gemini.go
+++ gemini/gemini.go
@@ -9,20 +9,18 @@ import (
 )
 
 const (
-	location  = "asia-northeast1"        // モデルのリージョン
-	modelName = "gemini-1.5-flash-002"   // 使用するGeminiモデル名
-	projectID = "term6-tomotaka-hoshina" // GCPプロジェクトID
+	location  = "asia-northeast1"
+	modelName = "gemini-1.5-flash-002"
+	projectID = "term6-tomotaka-hoshina"
 )
 
 func Translate(content string) string {
-	// Geminiクライアントの作成
 	ctx := context.Background()
 	client, err := genai.NewClient(ctx, projectID, location)
 	if err != nil {
 		return ""
 	}
 
-	// プロンプトを作成（信頼度スコアの取得）
 	prompt := genai.Text(fmt.Sprintf(
 		`次のツイートを「にゃ]を使って猫っぽくしてください。 %s`,
 		content))
@@ -33,7 +31,6 @@ func Translate(content string) string {
 		return ""
 	}
 
-	// レスポンスの確認
 	if len(resp.Candidates) == 0 || len(resp.Candidates[0].Content.Parts) == 0 {
 		return ""
 	}
@@ -46,13 +43,11 @@ func Translate(content string) string {
 	// レスポンスを print
 	fmt.Println(string(jsonData))
 
-	// JSON から "Parts" の値を取得する
 	var result map[string]interface{}
 	if err := json.Unmarshal(jsonData, &result); err != nil {
 		return ""
 	}
 
-	// "Parts" フィールドの取り出し
 	parts, ok := result["Candidates"].([]interface{})[0].(map[string]interface{})["Content"].(map[string]interface{})["Parts"].([]interface{})
 	if !ok || len(parts) == 0 {
 		return ""
